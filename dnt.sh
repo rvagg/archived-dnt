d18c51ce30dac9b784e956b79decd6787ffc0994
Rod Vagg <rod@vagg.org>
2013-11-25 14:33:57 +1100

    initial import

---
+++ dnt.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+##############################################################
+# DNT: Run tests in Docker containers for many Node versions #
+##############################################################
+
+CONFIG_FILE=".dntrc"
+OUTPUT_PREFIX=""
+COPYDIR=`pwd`
+# How many tests can we run at once without making the computer grind to a halt
+# or causing other unwanted resource problems:
+SIMULTANEOUS=`grep '^processor\s*\:\s*[0-9][0-9]*$' /proc/cpuinfo | wc -l`
+RSYNC_CMD="rsync -aAXx --delete --exclude .git --exclude build /dnt-src/ /dnt/"
+LOG_OK_CMD="tail -1"
+
+if [ `whoami` != "root" ]; then
+  echo "Must run dnt as root"
+  exit 1
+fi
+
+if [ -f $CONFIG_FILE ]; then
+  source ./$CONFIG_FILE
+else
+  echo "You must have a ${CONFIG_FILE} in the current directory"
+  exit 1
+fi
+
+# The versions of Node to test, this assumes that we have a Docker image
+# set up with the name "node_dev-<version>"
+#NODE_VERSIONS=`cat ${__dirname}/node_versions.list`
+
+if [ $# -gt 0 ] ; then
+  NODE_VERSIONS=$*
+  echo "Using Node versions: ${NODE_VERSIONS}"
+fi
+
+if [ "X${NODE_VERSIONS}" == "X" ]; then
+  echo "You must set up a NODE_VERSIONS setting in your ${CONFIG_FILE}"
+  exit 1
+fi
+
+START_TS=`date +%s`
+
+test_node() {
+  local OUT=/tmp/${OUTPUT_PREFIX}dnt-${NV}.out
+  local NV=$1
+  local ID=node_dev-$NV
+
+  docker inspect "$ID" &> /dev/null
+  if [[ $? -ne 0 ]]; then
+    echo -e "\033[31mCould not find container for [\033[1m$NV\033[22m]\033[39m"
+    return
+  fi
+
+  # Run test in a Docker container
+  docker run -v ${COPYDIR}:/dnt-src/:ro $ID /bin/bash -c " \
+    ${RSYNC_CMD}; \
+    ${TEST_CMD} \
+  " &> $OUT
+
+  # Print status
+  printf "Node@\033[1m\033[33m%-8s\033[39m\033[22m: " $NV
+  if [[ `cat /tmp/${OUTPUT_PREFIX}dnt-${NV}.out | $LOG_OK_CMD` == "ok" ]]; then
+    echo -ne "\033[1m\033[32mPASS\033[39m\033[22m"
+  else
+    echo -ne "\033[1m\033[31mFAIL\033[39m\033[22m"
+  fi
+  echo -e " \033[3mwrote output to ${OUT}\033[23m"
+}
+
+# Run all tests
+_C=0
+for NV in $NODE_VERSIONS; do
+  test_node $NV &
+  # Small break between each start, gives Docker breathing room
+  sleep 0.5
+
+  # Crude limiting on the number of simultaneous runs
+  let _C=_C+1
+  if [[ $((_C % ${SIMULTANEOUS})) == 0 ]]; then
+    wait
+  fi
+done
+
+wait
+
+END_TS=`date +%s`
+DURATION=$((END_TS-START_TS))
+VERSIONS=$(echo $NODE_VERSIONS | wc -w)
+
+echo "Took ${DURATION}s to run ${VERSIONS} versions of Node"
---
+++ setup-dnt.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+##########################################################
+# Set up Docker containers with basic dev tools and Node #
+##########################################################
+
+CONFIG_FILE=".dntrc"
+# For `make -j X`
+BUILD_JOBS=`grep '^processor\s*\:\s*[0-9][0-9]*$' /proc/cpuinfo | wc -l`
+
+if [ `whoami` != "root" ]; then
+  echo "Must run dnt as root"
+  exit 1
+fi
+
+if [ -f $CONFIG_FILE ]; then
+  source ./$CONFIG_FILE
+else
+  echo "You must have a ${CONFIG_FILE} in the current directory"
+  exit 1
+fi
+
+if [ $# -gt 0 ] ; then
+  NODE_VERSIONS=$*
+  echo "Using Node versions: ${NODE_VERSIONS}"
+fi
+
+if [ "X${NODE_VERSIONS}" == "X" ]; then
+  echo "You must set up a NODE_VERSIONS setting in your ${CONFIG_FILE}"
+  exit 1
+fi
+
+# Simple setup function for a container:
+#  setup_container(image id, base image, commands to run to set up)
+setup_container() {
+  local ID=$1
+  local BASE=$2
+  local RUN=$3
+
+  # Does this image exist? If yes, ignore
+  docker inspect "$ID" &> /dev/null
+  if [[ $? -eq 0 ]]; then
+    echo "Found existing container [$ID]"
+    return
+  fi
+
+  # No such image, so make it
+  echo "Did not find container container [$ID], creating..."
+  docker run $BASE /bin/bash -c "$RUN"
+  sleep 2
+  docker commit `docker ps -l -q` $ID
+}
+
+# A basic dev image with the build tools needed for Node
+setup_container "dev_base" "ubuntu" " \
+  apt-get update; \
+  apt-get install -y make gcc g++ python git"
+
+# The main Node repo in an image by itself
+setup_container "node_dev" "dev_base" " \
+  git clone https://github.com/joyent/node.git /usr/src/node/"
+
+# For each version of Node, make an image by checking out that branch
+# on the repo, building it and installing it
+for NV in $NODE_VERSIONS; do
+  setup_container "node_dev-$NV" "node_dev" " \
+    cd /usr/src/node && \
+    git fetch origin && \
+    git checkout $NV && \
+    git pull origin $NV && \
+    ./configure && \
+    make -j${BUILD_JOBS} && \
+    make install"
+done
